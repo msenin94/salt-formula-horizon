283550883edcb1c1d16ab7e49681e1c5d6dea439
Michael Senin <msenin@mirantis.com>
2017-03-05 00:42:12 +0400

    Fix RIL-751, RIL-807

---
+++ horizon/files/horizon_settings/_horizon_config.py
@@ -0,0 +1,26 @@
+# TODO (msenin): replace this construction on iteration by pillar data -
+# need to discuss this approach
+
+{%- set _horizon_config = salt['pillar.get']('horizon:server:horizon_config', {}) %}
+
+
+HORIZON_CONFIG = {
+    'user_home': 'openstack_dashboard.views.get_user_home',
+    'ajax_queue_limit': 10,
+    'auto_fade_alerts': {
+        'delay': 3000,
+        'fade_duration': 1500,
+        'types': ['alert-success', 'alert-info']
+    },
+    'help_url': "http://docs.openstack.org",
+    'exceptions': {'recoverable': exceptions.RECOVERABLE,
+                   'not_found': exceptions.NOT_FOUND,
+                   'unauthorized': exceptions.UNAUTHORIZED},
+    'modal_backdrop': 'static',
+    'angular_modules': [],
+    'js_files': [],
+    'js_spec_files': [],
+    'password_autocomplete': {{
+        _horizon_config.get('password_autocomplete', 'on')
+    }}
+}
